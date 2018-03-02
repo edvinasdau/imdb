5d2482ac4e8ee9f17a7e8fe950cc4d065033432e
vagrant <vagrant@homestead>
2018-03-02 12:37:10 +0000

    prideda kategorijas ir filmus is API

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -26,7 +26,7 @@ class MoviesController extends Controller
     public function edit($id){
         $cat = Category::get();
         $mo = Movie::all();
-            $mov = Movie::get();
+        $mov = Movie::get();
         $movie = Movie::findOrFail($id);
         return view('movies', ['movies' => $mo, 'data' => $movie, 'categories' => $cat, 'movies' => $mov]);
     }
@@ -61,34 +61,56 @@ class MoviesController extends Controller
     public function show_by_category(Request $request)
     {
         $categories = Category::all();
-         if($request->category_id == 'all'){
-             $movies = Movie::all();
-             $category = '';
-         }  else {
-             $category = Category::findOrFail($request->category_id);
-             $movies = $category->movies()->orderBy('name', 'asc')->get();
-         }
+        if($request->category_id == 'all'){
+            $movies = Movie::all();
+            $category = '';
+        }  else {
+            $category = Category::findOrFail($request->category_id);
+            $movies = $category->movies()->orderBy('name', 'asc')->get();
+        }
         return view('movies', ['movies' => $movies, 'cat' => $category, 'categories' => $categories]);
     }
 
     public function test()
     {
 
-        $filmai = json_decode(file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=be98f6d22107b703991b078fdf1aeb9c&language=en-US'));
+        $genres = json_decode(file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=be98f6d22107b703991b078fdf1aeb9c&language=en-US'));
 
-        //dd($filmai);
         $description = '';
-        foreach ($filmai->genres as $filmas){
-           $category_id = Category::create([
-               'name' => $filmas->name,
+        foreach($genres->genres as $genre) {
+
+           $category = Category::create([
+              'name' => $genre->name,
                'description' => $description,
                'user_id' => '5']);
+            $genre_id = $genre->id;
 
-           // $category_id->id
+            $filmai = json_decode(file_get_contents('https://api.themoviedb.org/3/genre/'. $genre_id .'/movies?api_key=be98f6d22107b703991b078fdf1aeb9c&language=en-US&include_adult=false&sort_by=created_at.asc'));
+//dd($filmai);
+            foreach($filmai->results as $filmas){
+
+                $date = explode('-', $filmas->release_date);
+                $date = $date[0];
+
+                Movie::create ([
+                    'name' => $filmas->original_title,
+                    'category_id' => $category->id,
+                    'user_id' => '5',
+                    'description' => $filmas->overview,
+                    'years' => $date,
+                    'rating' => $filmas->vote_average
+                ]);
+                //dd($filmas);
+            }
         }
 
-
-        //return view('/movies', ['movies' => $filmai, 'categories' => $categories] );
     }
 
+
 }
+
+// be98f6d22107b703991b078fdf1aeb9c
+
+//139.59.191.148
+
+// https://api.themoviedb.org/3/discover/movie?api_key=be98f6d22107b703991b078fdf1aeb9c&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=
